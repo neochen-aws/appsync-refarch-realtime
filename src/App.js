ada68c8c2c2d6b2ba78cb430de38c2e2df9773f3
neochen-aws <neochen@amazon.com>
2019-06-21 17:52:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -294,8 +294,9 @@ class App extends Component {
           <ReactPlayer
             className='react-player'
             url='https://d2qohgpffhaffh.cloudfront.net/HLS/vanlife/withad/sdr_uncage_vanlife_admarker_60sec.m3u8'
-            width='100%'
-            height='100%'
+            controls='true'
+            width='75%'
+            height='75%'
           />
         </div>
         <div className="container-fluid mx-auto">
